a79bb257b76eca207f96a574bfb66d2800342e43
nvega-ms <nvega@makingsense.com>
2019-08-02 15:43:12 -0300

    Added new test

--- tests/specs/test.spec.js
+++ tests/specs/test.spec.js
@@ -4,4 +4,9 @@ describe('webdriver.io page', function() {
         var title = browser.getTitle();
         expect(title, 'WebdriverIO - WebDriver bindings for Node.js');
     });
+    it('should have the right title - the fancy generator way', function () {
+        browser.url('http://webdriver.io');
+        var title = browser.getTitle();
+        expect(title, 'WebdriverIO - WebDriver bindings for Node.js 21');
+    });
 });
