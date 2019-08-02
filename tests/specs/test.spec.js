602e2d7d61ce9d7eaf014bbc88fd62f3dcafb9d9
nvega-ms <nvega@makingsense.com>
2019-08-02 15:50:49 -0300

    Added Jasmine syntax

--- tests/specs/test.spec.js
+++ tests/specs/test.spec.js
@@ -2,11 +2,11 @@ describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
         browser.url('http://webdriver.io');
         var title = browser.getTitle();
-        expect(title, 'WebdriverIO - WebDriver bindings for Node.js');
+        expect(title).toBe('WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
     it('should have the right title - the fancy generator way', function () {
         browser.url('http://webdriver.io');
         var title = browser.getTitle();
-        expect(title, 'WebdriverIO - WebDriver bindings for Node.js 21');
+        expect(title).toBe('WebdriverIO - WebDriver bindings for Node.js 21');
     });
 });
